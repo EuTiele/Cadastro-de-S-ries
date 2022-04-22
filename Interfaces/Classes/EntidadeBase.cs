dac381ae7905b6c726128c91527b261b0383824c
Tiele <tieletechnology@gmail.com>
2022-04-22 00:23:14 -0300

    Ok

---
+++ Interfaces/Classes/EntidadeBase.cs
@@ -0,0 +1,7 @@
+namespace Cadastro_de_S_ries
+{
+    public abstract class EntidadeBase
+    {
+        public int Id {get; protected set; }
+    }
+}
---
+++ Interfaces/Classes/Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace Cadastro_de_S_ries
+{
+    public enum Genero 
+    {
+        Ação = 1,
+        Aventura = 2,
+        Documentario = 3,
+        Comedia = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        Ficcao_Cientifica = 9,
+        Musical = 10,
+        Romance = 12,
+        Suspense = 12,
+        Terror = 13
+
+    }
+}
